ce4719bf5c35566b07149ce17950bfbfa759b40b
Chad Huneycutt <chad.huneycutt@gatech.edu>
2012-10-24 19:10:24 -0400

    allow override of authorized_keys file

--- manifests/authorizedkey.pp
+++ manifests/authorizedkey.pp
@@ -2,25 +2,32 @@
 # $srcuser@$srchost to login as $dstuser on *this host* ($::hostname)"
 # $srcuser is only used as a comment on the key, and as an identifier to locate
 # the key in the keystore (ie, the filename of the key)
-define sshkeys::authorizedkey (
-  $srcuser,
-  $srchost,
-  $dstuser,
-  $authorizedkey_file,
-  $keytype = 'rsa',
-) {
-   #FIXME This is a total hack.  sshkeys.pl returns a key in the form "options ssh-rsa KEY foo@bar"
-   # so we split them out here.  Really, we should refactor the sshkeys.pl script (or store them elsewhere)
-   $key = generate($sshkeys::install::scriptname, '--user', $srcuser, "--${keytype}", '--authkeys', $srchost, '--cmthost', $srchost)
-   $rawkeyarr = split($key,' ')
-   $keyopts = $rawkeyarr[0]
-   $rawkey = $rawkeyarr[2]
-   ssh_authorized_key { $name:
-     ensure => present,
-     key => $rawkey,
-     options => $keyopts,
-     user => $dstuser,
-     type => $keytype,
-     require => File[$sshkeys::install::scriptname]
-   }
+define sshkeys::authorizedkey ($srcuser, $srchost, $dstuser, $authorizedkey_file = 'UNSET', $keytype = 'rsa',) {
+  # FIXME This is a total hack.  sshkeys.pl returns a key in the form "options ssh-rsa KEY foo@bar"
+  # so we split them out here.  Really, we should refactor the sshkeys.pl script (or store them elsewhere)
+  $key = generate($sshkeys::install::scriptname, '--user', $srcuser, "--${keytype}", '--authkeys', $srchost, '--cmthost', $srchost)
+  $rawkeyarr = split($key, ' ')
+  $keyopts = $rawkeyarr[0]
+  $rawkey = $rawkeyarr[2]
+
+  if $authorizedkey_file == 'UNSET' {
+    ssh_authorized_key { $name:
+      ensure  => present,
+      key     => $rawkey,
+      options => $keyopts,
+      user    => $dstuser,
+      type    => $keytype,
+      require => File[$sshkeys::install::scriptname]
+    }
+  } else {
+    ssh_authorized_key { $name:
+      ensure  => present,
+      key     => $rawkey,
+      options => $keyopts,
+      user    => $dstuser,
+      target  => $authorizedkey_file,
+      type    => $keytype,
+      require => File[$sshkeys::install::scriptname]
+    }
+  }
 }

de66ac5bdfd94d4d298ba47043dddc015a45321e
Chad Huneycutt <chad.huneycutt@gatech.edu>
2012-10-31 18:52:43 -0400

    forgot to pass type to openssh::server::hostkey

--- manifests/hostkeys.pp
+++ manifests/hostkeys.pp
@@ -10,6 +10,7 @@ class sshkeys::hostkeys (
   openssh::server::hostkey { "${name}-rsa-private":
     key => generate($sshkeys::scriptname, '--private', '--rsa', '--host', $host),
     private => true,
+    type => 'rsa',
     mode => $privmode,
     owner => $owner,
     group => $group,
@@ -17,6 +18,7 @@ class sshkeys::hostkeys (
   openssh::server::hostkey { "${host}-rsa-public":
     key => generate($sshkeys::scriptname, '--rsa', '--host', $host),
     private => false,
+    type => 'rsa',
     mode => $pubmode,
     owner => $owner,
     group => $group,
@@ -24,6 +26,7 @@ class sshkeys::hostkeys (
   openssh::server::hostkey { "${name}-dsa-private":
     key => generate($sshkeys::scriptname, '--private', '--dsa', '--host', $host),
     private => true,
+    type => 'dsa',
     mode => $privmode,
     owner => $owner,
     group => $group,
@@ -31,6 +34,7 @@ class sshkeys::hostkeys (
   openssh::server::hostkey { "${host}-dsa-public":
     key => generate($sshkeys::scriptname, '--dsa', '--host', $host),
     private => false,
+    type => 'dsa',
     mode => $pubmode,
     owner => $owner,
     group => $group,

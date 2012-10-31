599a8c4e93a7b7d833047b908cdc78c5428b692b
Chad Huneycutt <chad.huneycutt@gatech.edu>
2012-10-31 18:47:37 -0400

    forgot private attribute to openssh::server::hostkey

--- manifests/hostkeys.pp
+++ manifests/hostkeys.pp
@@ -9,24 +9,28 @@ class sshkeys::hostkeys (
 
   openssh::server::hostkey { "${name}-rsa-private":
     key => generate($sshkeys::scriptname, '--private', '--rsa', '--host', $host),
+    private => true,
     mode => $privmode,
     owner => $owner,
     group => $group,
   }
   openssh::server::hostkey { "${host}-rsa-public":
     key => generate($sshkeys::scriptname, '--rsa', '--host', $host),
+    private => false,
     mode => $pubmode,
     owner => $owner,
     group => $group,
   }
   openssh::server::hostkey { "${name}-dsa-private":
     key => generate($sshkeys::scriptname, '--private', '--dsa', '--host', $host),
+    private => true,
     mode => $privmode,
     owner => $owner,
     group => $group,
   }
   openssh::server::hostkey { "${host}-dsa-public":
     key => generate($sshkeys::scriptname, '--dsa', '--host', $host),
+    private => false,
     mode => $pubmode,
     owner => $owner,
     group => $group,
